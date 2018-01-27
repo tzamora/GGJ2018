bf0bdf9de9a784833e9d96fb9c264afac71cce60
DESKTOP-0RE2D7R\eladi <eladiozro@gmail.com>
2018-01-27 13:10:08 -0600

    Go to Cross

---
+++ Assets/scripts/details/CrossAppearAndDestroy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using matnesis.TeaTime;
+
+public class CrossAppearAndDestroy : MonoBehaviour {
+
+    public float speed = 1;
+    public float time = 1;
+
+
+    void Start () {
+
+        MakeSmaller();
+
+    }
+	
+	void MakeSmaller () {
+
+        this.tt().Loop(time, delegate (ttHandler handler)
+        {
+            transform.localScale -= new Vector3(speed, speed, speed);
+
+        }).Add( t => { Destroy(gameObject);  } );
+
+	}
+}
