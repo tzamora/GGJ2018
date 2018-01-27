75059148913778a875cc95a8af3eaf9f1256960a
DESKTOP-0RE2D7R\eladi <eladiozro@gmail.com>
2018-01-27 02:45:30 -0600

    StartMenu

---
+++ Assets/scripts/StartBackGroundMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using matnesis.TeaTime;
+
+public class StartBackGroundMove : MonoBehaviour {
+
+    public float speed;
+    public float wait;
+
+    void Start () {
+
+        MoveRoutine();
+
+    }
+
+    void MoveRoutine() {
+
+        this.tt("MoveRoutine").Loop(wait, delegate (ttHandler handler) {
+
+            transform.Translate(speed, speed, 0);
+
+        }).Loop(wait, delegate( ttHandler handler) {
+
+            transform.Translate(-speed, -speed, 0);
+
+        }).Repeat();
+
+    }
+
+
+
+
+
+}
