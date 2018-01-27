e85730438a1b57fc315c5261a9dcfd74b451f808
DESKTOP-0RE2D7R\eladi <eladiozro@gmail.com>
2018-01-27 12:02:26 -0600

    characters

---
+++ Assets/scripts/ButtonChangeLevel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonChangeLevel : MonoBehaviour {
+
+	public void LoadLevel(string LevelName) {
+
+        SceneManager.LoadScene(LevelName);
+
+	}
+
+    public void QuitGame()
+    {
+
+        Application.Quit();
+
+    }
+
+}
