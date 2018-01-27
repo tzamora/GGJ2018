d782e708175661812940706774dfede71717a2b1
tzamora <tzamora@gmail.com>
2018-01-26 22:59:03 -0600

    insert art

---
+++ Assets/MasterController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using matnesis.TeaTime;
+using UnityEngine;
+
+public class MasterController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        float speed = 5;
+
+        float xAxis = 0;
+
+        float yAxis = 0;
+
+        this.tt("move").Loop((handler) => {
+
+            xAxis = Input.GetAxis("Horizontal");
+
+            yAxis = Input.GetAxis("Vertical");
+
+            this.transform.position = new Vector3(xAxis, yAxis, 0) * speed;
+
+        });
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+
+	}
+}
---
+++ Assets/scripts/GameContext.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using matnesis.TeaTime;
+
+public class GameContext : MonoSingleton<GameContext> {
+
+	public GameObject player;
+
+	public GameObject playerPrefab;
+
+	public GameObject DieMenuPanel;
+
+	public AudioClip ExplosionSound;
+
+	public AudioClip BackgroundSound;
+
+	public AudioClip ReactorSound;
+
+	public int EnergyBlocksDestroyed = 0;
+
+	public Transform startingPosition;
+
+	void Start()
+	{
+		
+		SoundManager.Get.PlayClip (BackgroundSound, true);
+	
+	}
+
+	public void CameraShakeRoutine(bool isVisible){
+
+		if (!isVisible)
+			return;
+
+		var currentCameraPosition = Camera.main.transform.position;
+
+		SoundManager.Get.PlayClip (ExplosionSound, false);
+
+		this.tt ().Add (0.01f, ()=>{
+
+			//
+			// move a little bit down
+			//
+
+			Camera.main.transform.position = Camera.main.transform.position + new Vector3(0f, -0.2f, 0f);
+
+		}).Add (0.01f, ()=>{
+
+			Camera.main.transform.position = Camera.main.transform.position + new Vector3(0f, 0.4f, 0f);
+
+		}).Add (0.01f, ()=>{
+
+			Camera.main.transform.position = Camera.main.transform.position + new Vector3(0.2f, 0f, 0f);
+
+		}).Add (0.01f, ()=>{
+
+			Camera.main.transform.position = Camera.main.transform.position + new Vector3(-0.4f, 0f, 0f);
+
+		}).Add (0.01f, ()=>{
+
+			Camera.main.transform.localPosition = Vector3.zero;
+
+		});
+
+	}
+
+	public void resetLevel(){
+	
+		Destroy (Camera.main.gameObject);
+
+		GameObject player = Instantiate(playerPrefab, GameContext.Get.startingPosition.position, Quaternion.identity);
+
+		//SceneManager.LoadScene(scene.name);
+		GameContext.Get.player = player;
+	
+	}
+}
