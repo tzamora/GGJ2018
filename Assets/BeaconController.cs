95aa513a5c6df10a81e9942761b8953a89457504
DESKTOP-0RE2D7R\eladi <eladiozro@gmail.com>
2018-01-28 08:28:08 -0600

    Commit Intro Con letras

--- Assets/BeaconController.cs
+++ Assets/BeaconController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.UI;
+using UnityEditor.SceneManagement;
+using matnesis.TeaTime;
 
 public class BeaconController : MonoBehaviour {
 
@@ -10,29 +13,66 @@ public class BeaconController : MonoBehaviour {
     public int requiredBeacons;
     public float speed;
     public float scalingSpeed;
+    public float fadeSpeed;
     public GameObject tilemapA;
     public GameObject tilemapB;
+    public Image fadeToWhite;
+
+    bool once=false;
 
     void Start () {
 		
 	}
 	
 	void Update () {
+
         if (allActivated == requiredBeacons)
-        { AllBeaconsReady(); }
+        { once = true; }
+
+        if (once == true)
+        {
+            AllBeaconsReady();
+            requiredBeacons = 999;
+            once = false;
+        }
     }
 
     void AllBeaconsReady()
     {
 
-        cameraAnimation.transform.position = 
-            Vector3.MoveTowards(cameraAnimation.transform.position, new Vector3(-20,50, cameraAnimation.transform.position.z), speed);
+        this.tt().Loop(4, delegate (ttHandler handler)
+        {
+            Destroy(cameraAnimation.GetComponent<MouseCameraMove>());
 
-        cameraAnimation.orthographicSize += scalingSpeed * Time.deltaTime;
 
-        tilemapA.GetComponent<TilemapRenderer>().maskInteraction = SpriteMaskInteraction.None;
-        tilemapB.GetComponent<TilemapRenderer>().maskInteraction = SpriteMaskInteraction.None;
+        }).Loop(30f, delegate (ttHandler handler)
+        {
 
-        Destroy(cameraAnimation.GetComponent<MouseCameraMove>());
+            cameraAnimation.transform.position =
+                Vector3.MoveTowards(cameraAnimation.transform.position, new Vector3(-20, 50, cameraAnimation.transform.position.z), speed);
+
+            cameraAnimation.orthographicSize += scalingSpeed * Time.deltaTime;
+
+            tilemapA.GetComponent<TilemapRenderer>().maskInteraction = SpriteMaskInteraction.None;
+            tilemapB.GetComponent<TilemapRenderer>().maskInteraction = SpriteMaskInteraction.None;
+
+        });
+
+
+        this.tt().Loop(20, delegate (ttHandler handler)
+        {
+
+
+        }).Loop(10f, delegate (ttHandler handler)
+        {
+
+            fadeToWhite.color = Color.Lerp
+                (fadeToWhite.color, new Vector4(1, 1, 1, 1), fadeSpeed * Time.deltaTime);
+
+        }).Add(delegate (ttHandler handler)
+        {
+
+            EditorSceneManager.LoadScene("StartMenu");
+        });
     }
 }
---
+++ Assets/scripts/details/Story.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using matnesis.TeaTime;
+
+public class Story : MonoBehaviour {
+
+    public string nextLevel;
+
+    public Image picA;
+    public Image picB;
+    public Image picC;
+    public Text txtA;
+    public Text txtB;
+    public Text txtC;
+
+    public Color txtColor;
+    public Color picColor;
+
+    public float speedSlowFade;
+    public float speedFastFade;
+    public float shortWait;
+    public float longWait;
+
+    public void Start()
+    {
+        txtA.color = Color.clear;
+        txtB.color = Color.clear;
+        txtC.color = Color.clear;
+        picA.color = Color.clear;
+        picB.color = Color.clear;
+        picC.color = Color.clear;
+
+        this.tt().Loop(0.5f, t =>
+        {
+
+        }).Loop(longWait, t =>
+        {
+            txtA.color = Color.Lerp(txtA.color, txtColor, speedSlowFade);
+            picA.color = Color.Lerp(picA.color, picColor, speedFastFade);
+
+        }).Loop(shortWait, t =>
+        {
+            txtA.color  = Color.Lerp(txtA.color, Color.clear, speedFastFade);
+            picA.color  = Color.Lerp(picA.color, Color.clear, speedSlowFade);
+
+        }).Loop(longWait, t =>
+        {
+            txtA.color = Color.clear;
+            picA.color = Color.clear;
+            txtB.color = Color.Lerp(txtB.color, txtColor, speedSlowFade);
+            picB.color = Color.Lerp(picB.color, picColor, speedFastFade);
+
+        }).Loop(shortWait, t =>
+        {
+            txtB.color  = Color.Lerp(txtB.color, Color.clear, speedFastFade);
+            picB.color  = Color.Lerp(picB.color, Color.clear, speedSlowFade);
+
+        }).Loop(longWait, t =>
+        {
+            txtB.color = Color.clear;
+            picB.color = Color.clear;
+            txtC.color = Color.Lerp(txtC.color, txtColor, speedSlowFade);
+            picC.color = Color.Lerp(picC.color, picColor, speedFastFade);
+
+        }).Loop(shortWait, t =>
+        {
+            txtC.color  = Color.Lerp(txtC.color, Color.clear, speedFastFade);
+            picC.color  = Color.Lerp(picC.color, Color.clear, speedSlowFade);
+
+        }).Add(t => SceneManager.LoadScene(nextLevel));
+
+    }
+}
