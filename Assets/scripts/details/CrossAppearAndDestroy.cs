ff7090a0ec5286f5eae46d1d1dbff88b287aa076
DESKTOP-0RE2D7R\eladi <eladiozro@gmail.com>
2018-01-27 19:39:42 -0600

    Stones_Progress_And_Look_Cross_Prefab

--- Assets/scripts/details/CrossAppearAndDestroy.cs
+++ Assets/scripts/details/CrossAppearAndDestroy.cs
@@ -7,9 +7,13 @@ public class CrossAppearAndDestroy : MonoBehaviour {
 
     public float speed = 0.1f;
     public float time = 1;
+    public float rotation = 1;
 
 
     void Start () {
+
+        int r = Random.Range(0, 360);
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, r));
 
         MakeSmaller();
 
@@ -20,6 +24,7 @@ public class CrossAppearAndDestroy : MonoBehaviour {
         this.tt().Loop(time, delegate (ttHandler handler)
         {
 
+            transform.Rotate(0,0, rotation);
             transform.localScale -= new Vector3(speed, speed, speed) * Time.deltaTime;
 
         }).Add( t => { Destroy(gameObject);  } );
---
+++ Assets/scripts/details/StoneLightAndParticles.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using matnesis.TeaTime;
+
+public class StoneLightAndParticles : MonoBehaviour {
+
+    public Color publicColorA = Color.red;
+    public Color publicColorB = Color.blue;
+    public Color publicColorC = Color.green;
+    public float duration = 6;
+    public float speed= 0.5f;
+    SpriteRenderer colorComponent;
+    public GameObject StoneLights;
+
+	void Start () {
+
+        colorComponent = GetComponent<SpriteRenderer>();
+        Animation();
+
+    }
+
+
+
+    void Animation()
+    {
+
+        Instantiate(StoneLights, transform.localPosition, Quaternion.identity);
+
+        this.tt().Loop(duration, delegate (ttHandler handler)
+        {
+
+            colorComponent.color = Color.Lerp(colorComponent.color, publicColorA, speed * Time.deltaTime);
+
+        }).Loop(duration, delegate (ttHandler handler)
+        {
+
+            colorComponent.color = Color.Lerp(colorComponent.color, publicColorB, speed * Time.deltaTime);
+
+        }).Loop(duration, delegate (ttHandler handler)
+        {
+
+            colorComponent.color = Color.Lerp(colorComponent.color, publicColorC, speed * Time.deltaTime);
+
+        }).Loop(duration, delegate (ttHandler handler)
+        {
+
+            // GET CAMERA CANVAS TEXTURE AND GET COLOR ALPHA FROM 0 TO 255
+
+        });
+
+    }
+}
